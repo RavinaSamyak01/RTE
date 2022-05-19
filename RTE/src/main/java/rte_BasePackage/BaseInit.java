1e41fa4e942588f3a72f949714bcb3e00d9523b3
rprajapati <rprajapati@SIPL92.SIPL.COM>
2022-05-19 15:05:39 +0530

    Changes of 19th May 2022

--- RTE/src/main/java/rte_BasePackage/BaseInit.java
+++ RTE/src/main/java/rte_BasePackage/BaseInit.java
@@ -531,7 +531,8 @@ public class BaseInit {
 
 		try {
 //			/kunjan.modi@samyak.com, pgandhi@samyak.com,parth.doshi@samyak.com
-			SendEmail.sendMail("ravina.prajapati@samyak.com", subject, msg.toString(), File);
+			SendEmail.sendMail("ravina.prajapati@samyak.com, asharma@samyak.com, parth.doshi@samyak.com", subject,
+					msg.toString(), File);
 
 			// SendEmail.sendMail("ravina.prajapati@samyak.com, asharma@samyak.com
 			// ,parth.doshi@samyak.com", subject, msg.toString(), File);
